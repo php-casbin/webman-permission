07ec75dd61eff46fb2ef4181c63e26b55fafdc02
万少波 <756684177@qq.com>
2022-06-01 16:21:40 +0800

    1、简化配置文件读取 2、策略模型支持多个，默认为第一个。其他扩展配置，只需要按照基础配置一样，复制一份，指定相关策略模型和适配器即可

--- src/config/plugin/casbin/webman-permission/permission.php
+++ src/config/plugin/casbin/webman-permission/permission.php
@@ -24,24 +24,5 @@ return [
             'rules_name' => null
         ],
     ],
-    // 其他扩展配置
-    'abac' => [
-        // 策略模型Model设置
-        'model' => [
-            'config_type' => 'file',
-            'config_file_path' => config_path() . '/plugin/casbin/webman-permission/abac-model.conf',
-            'config_text' => '',
-        ],
-        // 适配器
-        'adapter' => Casbin\WebmanPermission\Adapter\LaravelDatabaseAdapter::class,
-        // 数据库设置
-        'database' => [
-            // 数据库连接名称，不填为默认配置
-            'connection' => '',
-            // 策略表名（不含表前缀）
-            'rules_table' => 'casbin_rule',
-            // 策略表完整名称
-            'rules_name' => null
-        ],
-    ],
+    // 其他扩展配置，只需要按照基础配置一样，复制一份，指定相关策略模型和适配器即可
 ];
