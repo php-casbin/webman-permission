81eb0856ebc807bd72c9539642d75cf5b3f96980
lyt8384 <lyt8384@hotmail.com>
2023-02-28 22:18:28 +0800

    feat 补充单元测试

--- tests/LaravelDatabase/config/plugin/casbin/webman-permission/permission.php
+++ tests/LaravelDatabase/config/plugin/casbin/webman-permission/permission.php
@@ -19,5 +19,20 @@ return [
             'rules_name'  => null,
         ],
     ],
-    // 其他扩展配置，只需要按照基础配置一样，复制一份，指定相关策略模型和适配器即可
+    'other' => [
+        // 策略模型Model设置
+        'model' => [
+            'config_type'      => 'file',
+            'config_file_path' => __DIR__.'/rbac-model.conf',
+            'config_text'      => '',
+        ],
+        // 适配器
+        'adapter' => Casbin\WebmanPermission\Adapter\LaravelDatabaseAdapter::class, // Laravel 适配器
+        // 数据库设置
+        'database' => [
+            'connection'  => '',
+            'rules_table' => 'other_casbin_rule',
+            'rules_name'  => null,
+        ],
+    ],
 ];
